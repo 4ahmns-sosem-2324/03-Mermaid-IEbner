c541cd53567a02abe11f1f0abe41b156b96f2c03
Ina Ebner <ina-ebner@aon.at>
2024-02-28 10:19:36 +0100

    scripts added

---
+++ Assets/MyGame/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private float chaseSpeed;
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
